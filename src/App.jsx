771c5d1727d7673f53a35222b2af434e8a12cfb8
Vaigai Vendhan <133666759+vaigai138@users.noreply.github.com>
2024-10-20 18:33:19 +0530

    BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,12 @@ import HowItWorks from './components/HowItWorks';
 import Navbar from './components/Navbar';
 import WhatYouParking from './components/WhatYouParking';
 import Host from './components/Host/Host'; // Import Host component
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import routing tools
+import { BrowserRouter as Router, HashRouter, Route, Routes } from 'react-router-dom'; // Import routing tools
 import HostMain from './components/Host/HostMain';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="app">
         <Navbar />
         <Routes>
@@ -35,7 +35,7 @@ function App() {
           <Route path="/hostmain" element={<HostMain/>} />
         </Routes>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
